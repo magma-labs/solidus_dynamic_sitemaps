b8dc187c1b6d48726b17a8ec5edbf0a60be851b9
Jonathan Tapia <jonathan.tapia@magmalabs.io>
2019-02-08 14:58:35 -0600

    Add refactor

--- app/controllers/spree/sitemap_controller.rb
+++ app/controllers/spree/sitemap_controller.rb
@@ -1,42 +1,62 @@
-class Spree::SitemapController < Spree::BaseController
-  def index
-    @public_dir = root_url
-    @products = Spree::Product.available
-    @taxonomies = Spree::Taxonomy.all
-    @pages = _select_static_pages
+module Spree
+  class SitemapController < Spree::BaseController
+    before_action :load_public_url, only: [:index]
+    before_action :load_products, only: [:index]
+    before_action :load_taxonomies, only: [:index]
+    before_action :load_taxons, only: [:index]
+    before_action :load_pages, only: [:index]
 
-    respond_to do |format|
-      format.html {}
-      format.xml do
-        nav = _build_taxon_hash
-        nav = _build_pages_hash nav
-        nav = _add_products_to_tax nav, false
-        render layout: false, xml: _build_xml(nav)
-      end
-
-      format.text do
-        @nav = _add_products_to_tax(_build_taxon_hash, false)
-        render layout: false
+    def index
+      respond_to do |format|
+        format.html {}
+        format.xml { render xml: build_xml, layout: false }
+        format.text { render layout: false }
       end
     end
-  end
 
-  private
+    private
 
-  def _build_xml(nav)
-    ''.tap do |output|
+    def load_public_url
+      @public_url ||= root_url
+    end
+
+    def load_products
+      @products ||= Spree::Product.available
+    end
+
+    def load_taxonomies
+      @taxonomies ||= Spree::Taxonomy.all
+    end
+
+    def load_taxons
+      @taxons ||= Spree::Taxon.all
+    end
+
+    def load_pages
+      @pages ||= select_static_pages
+    end
+
+    def nav
+      nav = build_taxon_hash
+      nav = build_pages_hash(nav)
+      nav = add_products_to_tax(nav, false)
+
+      @nav ||= nav
+    end
+
+    def build_xml
       xml = Builder::XmlMarkup.new(target: output, indent: 2)
-      xml.instruct! :xml, version: "1.0", encoding: "UTF-8"
-      xml.urlset(xmlns: "http://www.sitemaps.org/schemas/sitemap/0.9" ) {
+      xml.instruct! :xml, version: '1.0', encoding: 'UTF-8'
+      xml.urlset(xmlns: 'http://www.sitemaps.org/schemas/sitemap/0.9' ) {
         xml.url {
-          xml.loc @public_dir
+          xml.loc @public_url
           xml.lastmod Date.today
           xml.changefreq 'daily'
           xml.priority '1.0'
         }
         nav.each do |_k, v|
           xml.url {
-            xml.loc @public_dir + v['link']
+            xml.loc @public_url + v['link']
             xml.lastmod v['updated'].xmlschema # change timestamp of last modified
             xml.changefreq 'weekly'
             xml.priority '0.8'
@@ -44,60 +64,54 @@ class Spree::SitemapController < Spree::BaseController
         end
       }
     end
-  end
 
-  def _build_taxon_hash
-    nav = {}
-
-    Spree::Taxon.all.each do |taxon|
-      tinfo = {}
-      tinfo['name'] = taxon.name
-      tinfo['depth'] = taxon.permalink.split('/').size
-      tinfo['link'] = 't/' + taxon.permalink
-      tinfo['updated'] = taxon.updated_at
-      nav[taxon.permalink] = tinfo
+    def build_taxon_hash
+      @taxons.each do |taxon|
+        {}.tap do |h|
+          h['name'] = taxon.name
+          h['depth'] = taxon.permalink.split('/').size
+          h['link'] = 't/' + taxon.permalink
+          h['updated'] = taxon.updated_at
+          nav[taxon.permalink] = h
+        end
+      end
     end
 
-    nav
-  end
-
-  def _add_products_to_tax(nav, _multiples_allowed)
-    Spree::Product.available.each do |product|
-      pinfo = {}
-      pinfo['name'] = product.name
-      pinfo['link'] = 'products/' + product.permalink	# primary
-      pinfo['updated'] = product.updated_at
-      nav[pinfo['link']] = pinfo # store primary
+    def add_products_to_tax(nav, _multiples_allowed)
+      @products.each do |product|
+        {}.tap do |h|
+          h['name'] = product.name
+          h['link'] = 'products/' + product.permalink	# primary
+          h['updated'] = product.updated_at
+          nav[h['link']] = h # store primary
+        end
+      end
     end
 
-    nav
-  end
+    def build_pages_hash(nav)
+      return nav if @pages.empty?
 
-  def _build_pages_hash(nav)
-    return nav if @pages.empty?
+      @pages.each do |page|
+        nav[page.slug] = { name: page.title,
+                           link: page.slug.gsub(/^\//, ''),
+                           updated: page.updated_at }
+      end
 
-    @pages.each do |page|
-      nav[page.slug] = { 'name' => page.title,
-                         'link' => page.slug.gsub(/^\//, ''),
-                         'updated' => page.updated_at }
+      nav
     end
 
-    nav
-  end
+    def select_static_pages
+      pages = []
 
-  def _select_static_pages
-    pages = []
+      if defined?(Spree::Page)
+        slugs_to_reject = ['/on-main-page']
 
-    begin
-      slugs_to_reject = ["/on-main-page"]
-
-      Spree::Page.visible.each do |page|
-        pages << page unless slugs_to_reject.include?(page.slug)
+        Spree::Page.visible.each do |page|
+          pages << page unless slugs_to_reject.include?(page.slug)
+        end
       end
 
       pages
-    rescue NameError
-      pages = []
     end
   end
 end
